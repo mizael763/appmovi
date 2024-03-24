6ab6e43b8f5388ca0f35dc9360df5f97a1201c75
Your Name <you@example.com>
2024-03-24 14:45:03 -0500

    main

--- flutter/flutter_application_1/lib/routes.dart
+++
@@ -1,4 +0,0 @@
-// TODO Implement this library.
-const String loginRoute = '/login';
-const String indexRoute = '/index';
-const String registerRoute = '/register';
--- flutter/flutter_application_2/lib/main.dart
+++ flutter/flutter_application_2/lib/main.dart
@@ -1,20 +1,102 @@
 import 'package:flutter/material.dart';
+import 'login/login_simple.dart'; 
+import 'login/login_simple2.dart'; 
+import 'login/login_simple3.dart'; 
+import 'login/login_simple4.dart'; 
+import 'login/login_simple5.dart'; 
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+      title: 'Selector de Formulario de Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const LoginPageSelector(),
+    );
+  }
+}
+
+class LoginPageSelector extends StatelessWidget {
+  const LoginPageSelector({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Selecciona tu Formulario de Login'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(15),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginSimple()),
+                  );
+                },
+                child: const Text('Formulario de Login Simple 1'),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginSimple2()),
+                  );
+                },
+                child: const Text('Formulario de Login Simple 2'),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15),
+              child:  ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginSimple3()),
+                  );
+                },
+                child: const Text('Formulario de Login Simple 3'),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15), // Aplica un margen de 15 en todos los lados del contenedor
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginSimple4()),
+                  );
+                },
+                child: const Text('Formulario de Login Simple 4'),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginSimple5()),
+                  );
+                },
+                child: const Text('Formulario de Login Simple 5'),
+              ),
+            ),
+          ],
         ),
       ),
     );
